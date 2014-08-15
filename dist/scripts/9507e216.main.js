a5c3f5f3b1c2eb63a64f539195f1a417c2d05591
Todd Heidenreich <jtheidenreich@gmail.com>
2014-08-15 16:08:51 -0400

    Initial dist subtree commit

---
+++ dist/scripts/9507e216.main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('a[href^="#"]').on("click",function(a){a.preventDefault();var b=this.hash,c=$(b);$("html, body").stop().animate({scrollTop:c.offset().top},900,"swing",function(){window.location.hash=b})})});
